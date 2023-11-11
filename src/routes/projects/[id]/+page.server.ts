e688b0e57d0ad9ead9e764545d696b8096294b76
MrEpoch <alexandrstencuk@seznam.cz>
2023-11-11 19:39:27 +0100

    Previus production failed cause it could not prerender projects/id so i made that route not prerendered

--- src/routes/projects/[id]/+page.server.ts
+++ src/routes/projects/[id]/+page.server.ts
@@ -1,6 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 import projects from '../projects';
 
+export const prerender = false;
+
 export const load = async ({ params }: { params: { id: string } }) => {
 	const project = projects.find((project) => project.id === params.id);
 	if (!project) {
